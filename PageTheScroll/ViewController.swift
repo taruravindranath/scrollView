8fd7c7740ecdc397aaf15dadbc5ae9c5b613aa3e
Tarun <Tarun@172-30-48-226.DYNAPOOL.NYU.EDU>
2017-09-05 20:13:50 -0400

    adding storyboard and viewcontroller

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    var images = [UIImageView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.addGestureRecognizer(scrollView.panGestureRecognizer)
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        
+        var contentWidth : CGFloat = 0.0
+        
+        print(scrollView.frame.size.width)
+        
+        let scrollViewWidth = scrollView.frame.size.width
+        
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            newX =  scrollViewWidth / 2 + scrollViewWidth * CGFloat(x)
+            
+            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height/2) - 75, width: 150, height: 150)
+        }
+        
+        scrollView.clipsToBounds = false
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        
+        print("count: \(images.count)")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
